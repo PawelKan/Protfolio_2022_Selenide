86253218a7cd71f39d66fe8b9aa727b69fb97e22
Paweł K <grylion02@gmail.com>
2022-07-12 16:47:21 +0200

    add remove single address

--- src/test/java/tests/Functional_MyAddressesTests.java
+++ src/test/java/tests/Functional_MyAddressesTests.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Condition;
 import com.github.javafaker.Faker;
 import models.ClientData;
 import models.Users;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import page_objects.*;
@@ -21,6 +23,7 @@ public class Functional_MyAddressesTests extends TestBase {
     MyAccount_MyAddressesPageObj myAddresses = new MyAccount_MyAddressesPageObj();
     MyAccount_MyAddressesUpdatePageObj myAddressesUpdate = new MyAccount_MyAddressesUpdatePageObj();
     Faker fakeData = new Faker();
+    ClientData clientData = new ClientData();
 
     @BeforeTest
     public void loginIntoApp() {
@@ -36,21 +39,20 @@ public class Functional_MyAddressesTests extends TestBase {
         CookiesHelpers.storeCookies(); // Store cookies for further tests
     }
 
-//    @BeforeClass
-//    public void loadCookiesForLogin(){
-//        CookiesHelpers.loadCookies();
-//    }
+    @BeforeMethod
+    public void loadCookiesForLogin(){
+        CookiesHelpers.loadCookies();
+    }
 
     @Test
     public void e2e_addNewUserAddressInMyAccount() {
-        ClientData clientData = new ClientData();
         String firstNameForTest = fakeData.name().firstName() + fakeData.name().firstName();
         clientData.setFirstNameUpdate(firstNameForTest);
 
         String aliasAddressNameForTest = fakeData.address().streetName();
         clientData.setAddressTitleAliasUpdate(aliasAddressNameForTest);
 
-        CookiesHelpers.loadCookies();
+        //CookiesHelpers.loadCookies();
         open(Urls.MY_ACCOUNT_PAGE);
         myAccount.getBtnMyAddresses().click();
         myAddresses.checkMyAddressesPageElements();
@@ -65,32 +67,45 @@ public class Functional_MyAddressesTests extends TestBase {
 
         myAddressesUpdate.getBtnSave().click();
 
-        myAddressesUpdate.getListOfAddresses().last().shouldHave(text(firstNameForTest)); // check address was added correctly
+        myAddresses.getListOfAddresses().last().shouldHave(text(firstNameForTest)); // check address was added correctly
     }
 
     @Test
     public void e2e_editUserAddressInMyAccount() {
-        ClientData clientData = new ClientData();
         String firstNameForTest = fakeData.name().firstName() + fakeData.name().firstName();
         clientData.setFirstNameUpdate(firstNameForTest);
 
-        CookiesHelpers.loadCookies();
+        //CookiesHelpers.loadCookies();
         open(Urls.MY_ACCOUNT_PAGE);
         myAccount.getBtnMyAddresses().click();
         myAddresses.checkMyAddressesPageElements();
         myAddresses.btnUpdate.click();
 
-        myAddressesUpdate.checkMyAddressesUpdatePageElements();
-        myAddressesUpdate.getBtnBackToYourAddress().click();
-
-        myAddresses.btnUpdate.click();
+        //myAddressesUpdate.checkMyAddressesUpdatePageElements();
+        //myAddressesUpdate.getBtnBackToYourAddress().click();
+        //myAddresses.btnUpdate.click();
 
         myAddressesUpdate.updateAddressForUser(clientData);
-
         myAddressesUpdate.getBtnSave().click();
-
         myAddresses.getSectionDefaultAddressSection().shouldHave(text(firstNameForTest));
     }
 
+    @Test
+    public void e2e_deleteUserAddressInMyAccount() {
+        //CookiesHelpers.loadCookies();
+        open(Urls.MY_ACCOUNT_PAGE);
+        myAccount.getBtnMyAddresses().click();
 
+        if (myAddresses.getListOfAddresses().size() > 1) {
+            //myAddresses.getListOfAddresses().last().find("ul").findAll("li").last().find("a[title=\"Delete\"]").click();
+            String addressText = myAddresses.getListOfAddresses().last().getText();
+
+            myAddresses.getListOfAddresses().last().find("li.address_update > a:nth-child(2)[title=\"Delete\"]").click();
+            myAddresses.acceptDeletionAddressAlert();
+            myAddresses.getListOfAddresses().last().shouldNotHave(Condition.text(addressText));
+
+        } else {
+            Assert.fail("Address panel shouldn't be empty and have more than 1 addresses");
+        }
+    }
 }
